b565e0140c0873841ec760e4a6342aef3ede9f79
luixaviles <luis.avilesz@gmail.com>
2019-11-23 09:56:30 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gdg-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'gdg-santacruz';
+}
